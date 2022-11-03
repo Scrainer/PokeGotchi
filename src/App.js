ddaf674c3db458e7d25f16d7626a82bfc130eb8a
César Carlier <cesarcarlier@hotmail.com>
2022-11-03 02:11:10 -0500

    Diseño inicial pokedex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
+import pokedexttop from './img/pokedex-01.png'
+import pokedextbottom from './img/pokedex-02.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Pokegotchi</h1>
+      <div className="left-container"></div>
+      <div className="pokedex-container">
+        <div className="top-pokedex">
+          <img src={pokedexttop}/>
+        </div>
+        <div className="screen-pokedex"></div>
+        <div className="bottom-pokedex">
+          <img src={pokedextbottom}/>
+        </div>
+      </div>
+      <div className="right-container"></div>
     </div>
   );
 }
